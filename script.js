80c98ef7df52e1f10fc9ebc669e30adebf4fdc49
ifwi4 <150092419+ifwi4@users.noreply.github.com>
2023-12-01 18:11:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,22 @@ function chat() {
   iframe.src = url.toString();
   win.document.body.appendChild(iframe);
 }
+function oldchat() {
+  var url = "https://email.aengine.repl.co";
+  var urlObj = new window.URL(window.location.href);
+  win = window.open();
+  win.document.body.style.margin = "0";
+  win.document.body.style.height = "100vh";
+  var iframe = win.document.createElement("iframe");
+  iframe.style.border = "none";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.margin = "0";
+  iframe.referrerpolicy = "no-referrer";
+  iframe.allow = "fullscreen";
+  iframe.src = url.toString();
+  win.document.body.appendChild(iframe);
+}
 function iframe() {
     var iframe_src = localStorage.getItem('iframe_src');
     var a, b, c;
